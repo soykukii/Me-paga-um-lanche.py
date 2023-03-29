7fc88375a18e02d574cc5ec94d4098ecc94eddae
kukii <125775759+soykukii@users.noreply.github.com>
2023-03-29 16:05:35 -0300

    Update Me-paga-um-lanche.py

--- Me-paga-um-lanche.py
+++ Me-paga-um-lanche.py
@@ -20,10 +20,10 @@ nao = tk.Button(root, text='Não')
 nao.place(x=140, y=80)
 nao.bind('<Enter>', hover)
 
-sim = tk.Button(root, text='Sim', command='msg')
+sim = tk.Button(root, text='Sim', command='msg') # A mensagem após apertar o botão "sim" não está funcionando
 sim.place(x=25, y=80, relx=0, rely=0)
 
 root.mainloop()
 
-# Código muito legal e funcional feito por mim...
-# Não precisa baixar a biblioteca 'tkinter' e nem a 'random', elas já são nativas do Python!
+# Comando feito por mim mesmo!
+# Não precisa baixar as bibliotecas 'tkinter' e 'random', elas ja são nativas do Python!
